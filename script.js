49536df7330663deefad628a66dc4e88fbe5a719
Blake Worsley <blakeworsley@gmail.com>
2016-07-18 17:19:09 -0600

    Edited CSS to look more like buttons description and refactored the javascript

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var $randomButtonClick = $('#random-button');
 var $resetButtonClick = $('#reset-button');
 var $layoutContainer = $('.layout-container');
 var hexArray = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
-var hexCreatedValue = hexArray[getRandomNumber(0,15)];
 
 $randomButtonClick.on('click', function() {
   $layoutContainer.css('background-color',randomBackgroundColor());
@@ -28,7 +27,7 @@ function randomGeneratedHex() {
 }
 
 function randomBackgroundColor() {
-if (randomGeneratedHex() != "#FFFFFF")
-  return randomGeneratedHex();
-else (randomGeneratedHex())
+  var color = randomGeneratedHex();
+  if (color !== "#FFFFFF") { return color; }
+  randomGeneratedHex();
 }
